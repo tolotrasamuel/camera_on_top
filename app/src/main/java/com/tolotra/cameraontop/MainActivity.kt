612ad0f0b335065bfc93290634b5aa548c17eeda
tolotrasamuel <tolotrasam@gmail.com>
2024-12-10 16:00:54 -0600

    Auto-commit modified files

--- app/src/main/java/com/tolotra/cameraontop/MainActivity.kt
+++ app/src/main/java/com/tolotra/cameraontop/MainActivity.kt
@@ -9,10 +9,7 @@ import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
-import android.util.DisplayMetrics
 import android.util.Rational
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
@@ -20,11 +17,6 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
 import com.google.common.util.concurrent.ListenableFuture
 import com.tolotra.cameraontop.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutionException
@@ -32,7 +24,6 @@ import java.util.concurrent.ExecutionException
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,15 +33,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.fab.setOnClickListener { view ->
-//            showOverlay();
             startCamera();
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
         }
 
         startCamera()
@@ -87,25 +71,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-//    private var mApplication: MyApplication? = null
-
-//
-//    private fun saveDpi() {
-//        val configuration: Configuration = getResources().getConfiguration()
-//        mApplication.orgDensityDpi = configuration.densityDpi
-//    }
-//
-//    private fun setDpi() {
-//        val configuration: Configuration = getResources().getConfiguration()
-//        val metrics: DisplayMetrics = getResources().getDisplayMetrics()
-//        if (mApplication.mode === MyApplication.MODE_PIP) {
-//            configuration.densityDpi = mApplication.orgDensityDpi / 3
-//        } else {
-//            configuration.densityDpi = mApplication.orgDensityDpi
-//        }
-//        getBaseContext().getResources().updateConfiguration(configuration, metrics)
-//    }
-
     private fun bindCameraPreview(cameraProvider: ProcessCameraProvider) {
 
 
@@ -118,7 +83,6 @@ class MainActivity : AppCompatActivity() {
         // Bind the preview use case and other needed user cases to a lifecycle
         val camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
 
-//        previewView.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
         previewView.implementationMode = PreviewView.ImplementationMode.PERFORMANCE
 
         // Create a surfaceProvider using the bound camera's cameraInfo
@@ -126,9 +90,6 @@ class MainActivity : AppCompatActivity() {
 
         // Attach the surfaceProvider to the preview use case to start preview
         preview.setSurfaceProvider(surfaceProvider)
-        // start the camera
-
-//        camera
     }
 
     protected override fun onUserLeaveHint() {
@@ -143,14 +104,9 @@ class MainActivity : AppCompatActivity() {
         super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
         if (isInPictureInPictureMode) {
             binding.fab.hide()
-//        if (isInPictureInPictureMode)
         } else {
             binding.fab.show()
         }
-//            mApplication.mode = MyApplication.MODE_PIP
-//        } else {
-//            mApplication.mode = MyApplication.MODE_FULL
-//        }
     }
 
     private fun checkOverlayPermission(): Boolean {
@@ -183,25 +139,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
